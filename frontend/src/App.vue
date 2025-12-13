1caa0a903b3064bee6963454ce8a720b029d0222
Csaba44 <csabi16sheldon@gmail.com>
2025-12-13 14:59:33 +0100

    Feature: tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,8 @@
-<script setup>
-  
-</script>
+<script setup></script>
 
 <template>
-  <nav>SkillIssue</nav>
-   <main>
+  <nav><h1 class="text-2xl underline font-bold bg-bgDark text-textWhite">SkillIssue</h1></nav>
+  <main>
     <RouterView />
   </main>
 </template>
