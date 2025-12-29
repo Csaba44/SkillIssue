996c0cc248345d6f44ed90650d669f79000a5344
Csaba44 <csabi16sheldon@gmail.com>
2025-12-29 11:34:49 +0100

    Refactor: button component - dynamic extension of classes, onClick added, maintainable code

---
+++ frontend/src/components/Generic/Button.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  title: String,
+  class: String,
+});
+
+const emit = defineEmits(["click"]);
+</script>
+
+<template>
+  <button @click="emit('click')" :class="'cursor-pointer bg-primary font-bold text-white py-2 px-10 rounded-full drop-shadow-lg transition-all duration-300 ease-out hover:scale-105 hover:shadow-2xl active:scale-95' + ' ' + props.class">
+    {{ title }}
+  </button>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,16 +2,20 @@
 import router from "../config/router";
 import { useUserStore } from "../stores/UserStore";
 import Navbar from "../components/HomePage/Navbar.vue";
-import CustomButton from "../components/Generic/CustomButton.vue";
-
+import Button from "../components/Generic/Button.vue";
 
 const userStore = useUserStore();
 </script>
 
 <template>
   <Navbar></Navbar>
-  <CustomButton title="Vágjunk bele"></CustomButton> <br>
-  {{ userStore.isAuthenticated ? `Hello, ${userStore.user.name}` : "Hello, guest" }}
-  <button v-if="userStore.isAuthenticated" @click="userStore.logout()">Logout</button>
-  <button v-else="userStore.isAuthenticated" @click="router.push('/login')">Login</button>
+  <div class="pt-2 px-5 md:px-10">
+    <Button @click="console.log('btn clicked')" class="text-sm" title="Vágjunk bele" />
+
+    <div>
+      {{ userStore.isAuthenticated ? `Hello, ${userStore.user.name}` : "Hello, guest" }}
+      <button v-if="userStore.isAuthenticated" @click="userStore.logout()">Logout</button>
+      <button v-else="userStore.isAuthenticated" @click="router.push('/login')">Login</button>
+    </div>
+  </div>
 </template>
