ce8cf54093cdbdc7ae0030464edbab6145374a75
Csaba44 <csabi16sheldon@gmail.com>
2025-12-30 15:09:31 +0100

    Feature: mobile view

--- frontend/src/components/Generic/Input.vue
+++ frontend/src/components/Generic/Input.vue
@@ -24,7 +24,7 @@ console.log(props.accentColor);
 </script>
 
 <template>
-  <div class="flex min-w-80 h-12 bg-bgDark border border-white/25 rounded-md" :class="props.class">
+  <div class="flex min-w-30 h-12 bg-bgDark border border-white/25 rounded-md" :class="props.class">
     <div v-if="props.icon" class="flex items-center justify-center px-2">
       <i :class="`${props.icon} text-${props.accentColor}`" class="text-xl"></i>
       <span :class="`bg-${props.accentColor}`" class="ml-2 h-6 w-0.5"></span>
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -29,16 +29,16 @@ onBeforeMount(async () => {
 
 <template>
   <div class="bg-bgDark w-screen h-screen p-8">
-    <div class="bg-bgAlternate w-full h-full rounded-md border border-white/25 grid grid-rows-1 grid-cols-2">
-      <div class="items-center justify-center border-r border-white/25 relative">
+    <div class="bg-bgAlternate w-full h-full rounded-md border border-white/25 grid grid-rows-3 grid-cols-1 md:grid-rows-1 md:grid-cols-2">
+      <div class="items-center justify-center border-b md:border-r border-white/25 relative">
         <div class="absolute inset-0 h-full w-full opacity-20 bg-[radial-gradient(#ffffff_1px,transparent_1px)] bg-size-[16px_16px] z-0"></div>
         <div class="z-10 grid grid-cols-1 grid-rows-5 p-10 h-full">
-          <div class="row-span-2 z-10 text-2xl md:text-5xl font-bold flex flex-col text-nowrap text-center gap-5 justify-center">
+          <div class="row-span-5 md:row-span-2 z-10 text-2xl md:text-5xl font-bold flex flex-col text-center gap-5 justify-center items-center">
             <span> <span class="text-white">Skill</span><span class="text-primary">Issue</span> </span>
             <span class="text-2xl text-textWhite">Játssz, tanulj és készülj az érettségire!</span>
           </div>
 
-          <div class="row-span-3 grid grid-rows-3 grid-cols-3 gap-5 z-10 items-center text-center">
+          <div class="hidden md:visible row-span-3 md:grid grid-rows-3 grid-cols-3 gap-5 z-10 items-center text-center">
             <div class="bg-accentYellow w-full h-full flex justify-center items-center rounded-lg">Államalapítás</div>
             <div class="bg-accentYellow w-full h-full flex justify-center items-center rounded-lg">Államalapítás</div>
             <div class="bg-accentYellow w-full h-full flex justify-center items-center rounded-lg">Államalapítás</div>
@@ -53,12 +53,12 @@ onBeforeMount(async () => {
           </div>
         </div>
       </div>
-      <div class="grid grid-cols-1 grid-rows-5 p-10 h-full items-center justify-center">
-        <div class="row-span-2 text-2xl md:text-5xl font-bold text-textWhite flex flex-col text-nowrap text-center gap-5 justify-center">Bejelentkezés</div>
+      <div class="row-span-2 md:row-span-1 grid grid-cols-1 grid-rows-5 p-10 h-full items-center justify-center">
+        <div class="row-span-2 text-2xl md:text-5xl font-bold text-textWhite flex flex-col text-center gap-5 justify-center">Bejelentkezés</div>
 
-        <form class="row-span-3 py-5 flex flex-col gap-3 items-center justify-start h-full">
-          <Input name="email" autocomplete="email" placeholder="gipsz.jakab@gmail.com" icon="fa-solid fa-at" class="bg-accentGreen" />
-          <Input name="password" autocomplete="password" type="password" placeholder="******" icon="fa-solid fa-key-skeleton" class="bg-accentGreen" />
+        <form class="row-span-3 py-5 flex flex-col gap-3 items-center justify-start h-full w-full">
+          <Input name="email" autocomplete="email" placeholder="gipsz.jakab@gmail.com" icon="fa-solid fa-at" class="w-full! lg:w-[50%]!" />
+          <Input name="password" autocomplete="password" type="password" placeholder="******" icon="fa-solid fa-key-skeleton" class="w-full! lg:w-[50%]!" />
           <Button title="Bejelentkezés" class="mt-3"></Button>
         </form>
       </div>
