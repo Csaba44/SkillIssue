00ed5c3e057afee5df0002fa6b3dd98722cd0660
FlenderDavid <flenderdavid07@gmail.com>
2025-12-30 14:59:33 +0100

    Feature: added topicCards

---
+++ frontend/src/components/Generic/TopicCard.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { defineProps } from "vue";
+
+const typeColors = {
+    history: { bg: "bg-accentYellow", stroke: "strokeYellow", strokeColor: "#988433" },
+    grammar: { bg: "bg-accentGreen", stroke: "strokeGreen", strokeColor: "#02805D" },
+    literature: { bg: "bg-accentPurple", stroke: "strokePurple", strokeColor: "#6146AC" },
+};
+
+const props = defineProps({
+    icon: String,
+    iconTop: Number,
+    iconLeft: Number,
+    type: String,
+    text: String
+});
+
+const getStrokeColor = (type) => typeColors[type]?.strokeColor || "#ffffff";
+</script>
+
+<template>
+    <div class="relative overflow-hidden flex items-center m-1 h-30 w-70 justify-center rounded-2xl"
+        :class="typeColors[props.type].bg">
+        <i class="absolute z-10 text-8xl"
+            :class="[icon, `top-[${props.iconTop}px]`, `left-[${props.iconLeft}px]`]"
+            :style="{ color: getStrokeColor(props.type) }"></i>
+        <p class="font-bold text-white text-center z-50 text-3xl" :class="typeColors[props.type].stroke">
+            {{ text }}
+        </p>
+    </div>
+</template>
--- frontend/src/components/HomePage/Header.vue
+++ frontend/src/components/HomePage/Header.vue
@@ -1,17 +1,29 @@
 <script setup>
 import Button from "../Generic/Button.vue";
 import Container from "../Generic/Container.vue";
+import TopicCard from "../Generic/TopicCard.vue";
 </script>
 
 <template>
-  <div id="home" class="absolute w-full min-h-full bg-bgDark grid md:grid-cols-2 md:grid-rows-1 grid-cols-1 grid-rows-2 md:items-center overflow-hidden">
-    <Container class="mt-30 md:mt-0 h-full flex flex-col md:items-start items-center text-center md:text-left justify-center">
+  <div id="home"
+    class="absolute w-full min-h-full bg-bgDark grid md:grid-cols-2 md:grid-rows-1 grid-cols-1 grid-rows-2 md:items-center overflow-hidden">
+    <Container
+      class="mt-30 md:mt-0 h-full flex flex-col md:items-start items-center text-center md:text-left justify-center">
       <i class="fa-solid fa-graduation-cap text-[500px] text-accentPurple opacity-5 absolute top-50 left-15"></i>
       <h1 class="text-3xl md:text-7xl font-bold text-white text-nowrap">Érettségire fel!</h1>
       <h2 class="text-lg md:text-2xl mt-3 font-bold text-white w-full">Játssz, tanulj és készülj az érettségire!</h2>
       <Button title="Vágjunk bele" class="mt-8 md:text-lg text-md text-nowrap" />
     </Container>
 
-    <Container class="h-full flex flex-col md:items-end items-center justify-center"><p class="text-white">TopicCards</p></Container>
+    <Container class="h-full flex justify-center items-center p-10">
+      <div class="grid gap-6 sm:grid-cols-1 md:grid-cols-3 w-full">
+
+        <TopicCard icon="fa-brands fa-font-awesome" :iconTop="20" :iconLeft="1" type="history" text="Államalapítás" />
+        <TopicCard icon="fa-solid fa-book" :iconTop="20" :iconLeft="1" type="grammar" text="Nyelvtan" />
+        <TopicCard icon="fa-solid fa-flask" :iconTop="20" :iconLeft="1" type="literature" text="Irodalom" />
+      </div>
+    </Container>
+
+
   </div>
 </template>
