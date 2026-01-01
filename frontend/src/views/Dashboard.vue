3a45e1400f7b6b3ac0360ccf61aee4d93d37fdad
Csaba44 <csabi16sheldon@gmail.com>
2026-01-01 16:40:12 +0100

    Fix: small design adjustment

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -37,7 +37,7 @@ const stopMatchmaking = () => {
 
 <template>
   <ProtectedPageContainer class="relative overflow-hidden">
-    <i class="fa-solid fa-graduation-cap rotate-30 text-accentPurple text-[2180px] absolute z-0 opacity-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></i>
+    <i class="fa-solid fa-graduation-cap rotate-30 text-accentPurple text-[2190px] absolute z-0 opacity-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></i>
     <Navbar :is-matchmaking="isMatchmaking" @selected-gamemode-change="(mode) => (selectedGameMode = mode)" />
 
     <Widget class="mt-10">
