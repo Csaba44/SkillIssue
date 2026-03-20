484867b55329290b9c3ae6133bbdafad8fecf65a
FlenderDavid <flenderdavid07@gmail.com>
2026-03-20 11:42:57 +0100

    Fix: homepage sections

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -11,8 +11,8 @@ import PricingCard from "../components/HomePage/PricingCard.vue";
   <Navbar></Navbar>
   <Header></Header>
 
-  <section id="about">
-    <div class="relative h-full bg-gray-100 xl:pb-20 pb-12">
+  <div class="relative h-full bg-gray-100 xl:pb-20 pb-12">
+
       <div class="z-0 top-0 left-0 block leading-none w-screen">
         <svg class="w-screen h-auto block" width="1920" height="242" viewBox="0 0 1920 242" fill="none"
           xmlns="http://www.w3.org/2000/svg">
@@ -20,8 +20,10 @@ import PricingCard from "../components/HomePage/PricingCard.vue";
             d="M0 242L80 213.675C160 185.047 320 128.853 480 128.625C640 128.853 800 185.047 960 205.55C1120 226.053 1280 209.347 1440 209.575C1600 209.347 1760 226.053 1840 233.875L1920 242V-1H1840C1760 -1 1600 -1 1440 -1C1280 -1 1120 -1 960 -1C800 -1 640 -1 480 -1C320 -1 160 -1 80 -1H0V242Z"
             fill="#030F39" />
         </svg>
-        <h1 class="text-6xl pb-5 font-bold text-bgDark text-nowrap text-right mr-13">Rólunk</h1>
+        <h1 id="about" class="text-6xl pb-5 font-bold text-bgDark text-nowrap text-right mr-13">Rólunk</h1>
       </div>
+
+
 
       <Container class="w-full h-min relative flex items-center justify-center z-10">
         <div class="grid gap-6 grid-cols-1 md:grid-cols-2 sm:grid-cols-2 xl:grid-cols-4">
@@ -39,15 +41,17 @@ import PricingCard from "../components/HomePage/PricingCard.vue";
             class="md:mt-50 md:bottom-30 xl:bottom-0" :type="'yellow'"></AboutCard>
         </div>
       </Container>
-      <div class="absolute inset-0 flex items-end justify-start z-0 pointer-events-none">
-        <span class="font-bold text-gray-400 opacity-30 text-4xl sm:text-6xl md:text-8xl xl:text-9xl"> SkillIssue.
-        </span>
-      </div>
+    <div class="absolute inset-0 flex items-end justify-start z-0 pointer-events-none">
+      <span class="font-bold text-gray-400 opacity-30 text-4xl sm:text-6xl md:text-8xl xl:text-9xl"> SkillIssue.
+      </span>
+
     </div>
-  </section>
+
+  </div>
 
 
-  <section class="pt-20 h-full bg-bgDark text-white pb-16 relative overflow-hidden">
+
+  <section id="services" class="pt-20 h-full bg-bgDark text-white pb-16 relative overflow-hidden">
     <Container>
       <h2 class="lg:text-5xl text-3xl md:text-4xl text-nowrap text-right font-bold tracking-wide mb-12">SZOLGÁLTATÁSAINK
       </h2>
