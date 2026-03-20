db2e009f3688835d975874fbf47fc28bde193fa0
FlenderDavid <flenderdavid07@gmail.com>
2026-03-20 12:01:03 +0100

    Feature: admin navitem

--- frontend/src/components/Dashboard/Navbar.vue
+++ frontend/src/components/Dashboard/Navbar.vue
@@ -39,7 +39,7 @@ const logoutClicked = async () => {
   toast.promise(userStore.logout(), {
     loading: "Kijelentkezés folyamatban...",
     success: () => {
-      router.push("/"); 
+      router.push("/");
       return "Sikeres kijelentkezés";
     },
     error: "Hiba történt a kijelentkezéskor.",
@@ -55,9 +55,10 @@ const eloToNextRank = computed(() => {
 <template>
   <div class="w-full flex justify-center items-center">
     <Widget class="rounded-full! px-10 py-4 w-full max-w-6xl grid grid-cols-1 md:grid-cols-3 items-center relative">
-      
+
       <div class="text-center md:text-left">
-        <RouterLink to="/" class="text-xl text-white font-bold">Skill<span class="text-primary">Issue</span></RouterLink>
+        <RouterLink to="/" class="text-xl text-white font-bold">Skill<span class="text-primary">Issue</span>
+        </RouterLink>
       </div>
 
       <div class="hidden md:flex justify-center">
@@ -71,39 +72,45 @@ const eloToNextRank = computed(() => {
               class="h-full bg-gradient-to-r from-accentPurple via-accentGreen to-accentGreen transition-all duration-500"
               :style="{
                 width: ((user.elo - user.rank.min_elo) / (user.next_rank.min_elo - user.rank.min_elo)) * 100 + '%',
-              }"
-            ></div>
+              }"></div>
           </div>
 
-          <div class="absolute top-10 bg-bgDark text-textWhite text-xs px-3 py-1 rounded-md opacity-0 scale-95 group-hover:opacity-100 group-hover:scale-100 transition-all duration-200 pointer-events-none whitespace-nowrap">
+          <div
+            class="absolute top-10 bg-bgDark text-textWhite text-xs px-3 py-1 rounded-md opacity-0 scale-95 group-hover:opacity-100 group-hover:scale-100 transition-all duration-200 pointer-events-none whitespace-nowrap">
             {{ eloToNextRank }} Elo a következő rangig
           </div>
         </div>
       </div>
 
       <div class="flex justify-center md:justify-end items-center gap-6 mt-4 md:mt-0">
-        
+
         <div v-if="!minimal" class="flex items-center gap-2">
           <span class="text-lg font-medium">{{ user.streak_count }}</span>
           <i class="fa-solid fa-fire text-primary text-lg"></i>
         </div>
 
         <div v-if="isAuthenticated" class="relative" ref="dropdownRef">
-          <button 
-            @click.stop="isUserDropdownVisible = !isUserDropdownVisible" 
-            class="text-lg font-medium hover:text-primary cursor-pointer transition-colors"
-          >
+          <button @click.stop="isUserDropdownVisible = !isUserDropdownVisible"
+            class="text-lg font-medium hover:text-primary cursor-pointer transition-colors">
             {{ user.name }}
             <i class="fa-solid fa-chevron-down text-xs ml-1 opacity-50"></i>
           </button>
 
           <transition name="dropdown">
-            <div v-if="isUserDropdownVisible" class="absolute right-0 mt-4 bg-bgAlternate rounded-xl w-44 p-4 flex flex-col gap-2 shadow-2xl border border-white/5 z-[100]">
+            <div v-if="isUserDropdownVisible"
+              class="absolute right-0 mt-4 bg-bgAlternate rounded-xl w-44 p-4 flex flex-col gap-2 shadow-2xl border border-white/5 z-[100]">
               <RouterLink :to="'/profiles/' + user.id">
-                <button class="text-left hover:text-warning transition w-full cursor-pointer" @click="isUserDropdownVisible = false">Profilom</button>
+                <button class="text-left hover:text-warning transition w-full cursor-pointer"
+                  @click="isUserDropdownVisible = false">Profilom</button>
               </RouterLink>
 
-              <button @click="logoutClicked" class="text-left text-error hover:opacity-80 transition w-full cursor-pointer">Kijelentkezés</button>
+              <RouterLink v-if="userStore.user.is_admin" to="/admin">
+                <button class="text-left hover:text-warning transition w-full cursor-pointer"
+                  @click="isUserDropdownVisible = false">Admin panel</button>
+              </RouterLink>
+
+              <button @click="logoutClicked"
+                class="text-left text-error hover:opacity-80 transition w-full cursor-pointer">Kijelentkezés</button>
             </div>
           </transition>
         </div>
@@ -113,10 +120,13 @@ const eloToNextRank = computed(() => {
 </template>
 
 <style scoped>
-.dropdown-enter-active, .dropdown-leave-active {
+.dropdown-enter-active,
+.dropdown-leave-active {
   transition: all 0.2s ease;
 }
-.dropdown-enter-from, .dropdown-leave-to {
+
+.dropdown-enter-from,
+.dropdown-leave-to {
   opacity: 0;
   transform: translateY(-10px);
 }
