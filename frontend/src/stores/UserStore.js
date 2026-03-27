f44ae4f68e5269b1b9c380f2829b9ce8c4752402
FlenderDavid <flenderdavid07@gmail.com>
2026-03-27 12:19:50 +0100

    Fix: login-logout error message

--- frontend/src/stores/UserStore.js
+++ frontend/src/stores/UserStore.js
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 import api from "../config/api";
+import { socket } from "../config/websocket";
 
 export const useUserStore = defineStore("counter", {
   state: () => {
@@ -14,6 +15,8 @@ export const useUserStore = defineStore("counter", {
         const response = await api.get("/api/users");
         this.user = response.data;
         this.isAuthenticated = true;
+
+        if (!socket.connected) socket.connect();
       } catch (error) {
         console.log(error);
         if (!error.response) return console.error("Check your internet connection.");
@@ -27,6 +30,7 @@ export const useUserStore = defineStore("counter", {
       try {
         await api.get("/api/csrf-cookie");
         const response = await api.post("/api/logout");
+        socket.disconnect();
         await this.verifySession();
       } catch (error) {
         if (!error.response) return console.error("Check your internet connection.");
