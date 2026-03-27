97a9f6fd8b52d3d62c4d4992fe280b896c65dec2
FlenderDavid <flenderdavid07@gmail.com>
2026-03-27 11:05:24 +0100

    Fix: pricing card disabled button

--- frontend/src/components/Generic/Button.vue
+++ frontend/src/components/Generic/Button.vue
@@ -11,7 +11,7 @@ const emit = defineEmits(["click"]);
 </script>
 
 <template>
-  <button :disabled="props.disabled" @click="emit('click')" :class="'cursor-pointer bg-primary disabled:bg-textDisabled! font-bold text-white py-2 px-10 rounded-full drop-shadow-lg transition-all duration-300 ease-out enabled:hover:scale-105 enabled:hover:shadow-2xl enabled:active:scale-95 disabled:transition-none' + ' ' + props.class">
+  <button :disabled="props.disabled" @click="emit('click')" :class="'cursor-pointer disabled:cursor-default bg-primary disabled:bg-textDisabled! font-bold text-white py-2 px-10 rounded-full drop-shadow-lg transition-all duration-300 ease-out enabled:hover:scale-105 enabled:hover:shadow-2xl enabled:active:scale-95 disabled:transition-none' + ' ' + props.class">
     {{ title }}
   </button>
 </template>
