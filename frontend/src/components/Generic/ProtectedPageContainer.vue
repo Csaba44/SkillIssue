c6b3efa63d514032c4ba0d14282c5caaaa317806
Csaba44 <csabi16sheldon@gmail.com>
2026-03-30 10:28:01 +0200

    Feature: container

--- frontend/src/components/Generic/ProtectedPageContainer.vue
+++ frontend/src/components/Generic/ProtectedPageContainer.vue
@@ -5,8 +5,8 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="bg-bgDark w-screen min-h-screen p-8 flex">
-    <div class="bg-bgAlternate w-full rounded-md border border-white/25 min-h-full p-5" :class="props.class">
+  <div class="bg-bgDark w-screen min-h-screen p-0 md:p-8 flex">
+    <div class="bg-bgAlternate w-full md:rounded-md border md:border-white/25 min-h-full p-2 md:p-5" :class="props.class">
       <slot></slot>
     </div>
   </div>
